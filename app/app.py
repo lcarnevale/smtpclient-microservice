57b3606be30ca1a281ffd9fe9cbbb2571dbfb6f2
Lorenzo Carnevale <lorenzocarnevale@gmail.com>
2019-06-13 19:15:01 +0200

    commented

--- app/app.py
+++ app/app.py
@@ -1,12 +1,21 @@
-# thierd parties libraries
+""" Web Application
+
+.. _Google Python Style Guide
+    https://github.com/google/styleguide/blob/gh-pages/pyguide.md
+"""
+
+__copyright__ = 'Copyright 2019, University of Messina'
+__author__ = 'Lorenzo Carnevale <lorenzocarnevale@gmail.com>'
+
+# third parties libraries
 from flask import Flask
 from flask_restful import Api
-from resources.smtp import SMTPGmailServer
+from resources.smtp import SMTPGmailClient
 
 app = Flask(__name__)
 api = Api(app)
 
-api.add_resource(SMTPGmailServer, '/')
+api.add_resource(SMTPGmailClient, '/')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
--- app/resources/smtp.py
+++ app/resources/smtp.py
@@ -1,3 +1,12 @@
+""" SMTP Client Resources Collection
+
+.. _Google Python Style Guide
+    https://github.com/google/styleguide/blob/gh-pages/pyguide.md
+"""
+
+__copyright__ = 'Copyright 2019, University of Messina'
+__author__ = 'Lorenzo Carnevale <lorenzocarnevale@gmail.com>'
+
 # standard libraries
 import yaml
 import smtplib
@@ -6,11 +15,16 @@ from flask import request
 from flask_restful import Resource
 
 
-class SMTPGmailServer(Resource):
-    """
+class SMTPGmailClient(Resource):
+    """ Gmail SMTP Client
+
+    Args:
+        __username (str): Gmail account;
+        __password (str): Gmail account's password;
+        __sent_from (str): mail's sender.
     """
     def __init__(self):
-        """
+        """ Constructor
         """
         with open('conf.yaml', 'r') as f:
             conffile = yaml.load(f, Loader=yaml.Loader)
@@ -23,7 +37,7 @@ class SMTPGmailServer(Resource):
 
 
     def __create_email(self):
-        """
+        """ Sets up mail metadata
         """
         json_data = request.get_json(force=True)
 
@@ -35,7 +49,7 @@ class SMTPGmailServer(Resource):
         return sent_from, to, body
 
     def post(self):
-        """
+        """ HTTP POST request to send a mail
         """
         server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
         # server.ehlo()
